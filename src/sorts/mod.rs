f0b43f5cb4c04885b806c6f8a80fedd939497714
Emmanuel Goh <emman27@fb.com>
2020-09-16 21:33:01 +0800

    Add reversed insertion sort

--- src/sorts/mod.rs
+++ src/sorts/mod.rs
@@ -1,14 +1,23 @@
-pub fn insertion<T: std::cmp::Ord>(collection: &mut std::vec::Vec<T>) -> &std::vec::Vec<T>
+pub enum SortOrder {
+    ASCENDING,
+    DESCENDING,
+}
+
+pub fn insertion<T>(collection: &mut std::vec::Vec<T>, order: SortOrder) -> &std::vec::Vec<T>
 where
     T: std::cmp::Ord,
     T: std::clone::Clone,
     T: std::fmt::Debug,
 {
+    let cmp = match order {
+        SortOrder::ASCENDING => std::cmp::Ordering::Greater,
+        SortOrder::DESCENDING => std::cmp::Ordering::Less,
+    };
     let len = collection.len();
     for j in 1..len {
         let key = collection[j].clone();
         let mut i = j as isize - 1;
-        while i >= 0 && collection[i as usize] > key {
+        while i >= 0 && collection[i as usize].cmp(&key) == cmp {
             collection[i as usize + 1] = collection[i as usize].clone();
             i -= 1;
         }
--- src/sorts/tests.rs
+++ src/sorts/tests.rs
@@ -1,31 +1,51 @@
+use super::SortOrder;
+
 #[test]
 fn empty_collection() {
     let mut v: std::vec::Vec<i32> = vec![];
-    super::insertion(&mut v);
+    super::insertion(&mut v, SortOrder::ASCENDING);
     assert_eq!(v, vec![]);
 }
 
 #[test]
 fn sorted() {
     let mut v: std::vec::Vec<i32> = vec![1, 2, 3];
-    super::insertion(&mut v);
+    super::insertion(&mut v, SortOrder::ASCENDING);
     assert_eq!(v, vec![1, 2, 3]);
 }
 
 #[test]
 fn unsorted() {
     let mut v: std::vec::Vec<i32> = vec![3, 2, 1];
-    assert_eq!(*super::insertion(&mut v), vec![1, 2, 3]);
+    assert_eq!(
+        *super::insertion(&mut v, SortOrder::ASCENDING),
+        vec![1, 2, 3]
+    );
 }
 
 #[test]
 fn book() {
     let mut v: std::vec::Vec<i32> = vec![5, 2, 4, 6, 1, 3];
-    assert_eq!(*super::insertion(&mut v), vec![1, 2, 3, 4, 5, 6]);
+    assert_eq!(
+        *super::insertion(&mut v, SortOrder::ASCENDING),
+        vec![1, 2, 3, 4, 5, 6]
+    );
 }
 
 #[test]
 fn string() {
     let mut v: std::vec::Vec<&str> = vec!["x", "a", "c"];
-    assert_eq!(*super::insertion(&mut v), vec!["a", "c", "x"]);
+    assert_eq!(
+        *super::insertion(&mut v, SortOrder::ASCENDING),
+        vec!["a", "c", "x"]
+    );
+}
+
+#[test]
+fn reversed() {
+    let mut v: std::vec::Vec<i32> = vec![5, 2, 4, 6, 1, 3];
+    assert_eq!(
+        *super::insertion(&mut v, SortOrder::DESCENDING),
+        vec![6, 5, 4, 3, 2, 1]
+    );
 }
